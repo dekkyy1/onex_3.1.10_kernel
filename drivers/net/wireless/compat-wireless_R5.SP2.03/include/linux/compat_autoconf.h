4ff23221139bbae3b49b35eacfa91cc798fa52af
Xmister <xmisterhu@gmail.com>
2013-02-07 16:14:29 +0100

    made some changes

--- drivers/net/wireless/compat-wireless_R5.SP2.03/include/linux/compat_autoconf.h
+++ drivers/net/wireless/compat-wireless_R5.SP2.03/include/linux/compat_autoconf.h
@@ -2,7 +2,11 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
+HEAD
  * Thu Feb  7 21:42:57 CET 2013 
+=======
+ * 2013. febr.  7., csütörtök, 16.05.17 CET 
+c07e177... CPU tuning and other fixes
  * compat-wireless-2.6: ol_R5.00.14
  * linux-2.6: ol_R5.00.21-23-g618c7e5
  */
